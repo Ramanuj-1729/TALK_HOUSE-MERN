d059c5dcdc0e5675d9a3bd4e6ad7ad5c79d82166
Ramanuj <75874452+Ramanuj-1729@users.noreply.github.com>
2023-02-19 02:43:42 +0530

    TextInput,StepOtp,StepPhoneEmail,Phone,Email

--- frontend/src/pages/Steps/StepOtp/StepOtp.jsx
+++ frontend/src/pages/Steps/StepOtp/StepOtp.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Card from '../../../components/shared/Card/Card';
+import TextInput from '../../../components/shared/TextInput/TextInput';
+import Button from '../../../components/shared/Button/Button';
 import styles from './StepOtp.module.css';
 
 const StepOtp = () => {
+    const [otp, setOtp] = useState('');
     return (
-        <div>StepOtp</div>
+        <>
+            <div className={styles.cardWrapper}>
+                <Card
+                    title="Enter the code we just texted you"
+                    icon="lock-emoji"
+                >
+                    <TextInput
+                        value={otp}
+                        onChange={(e) => setOtp(e.target.value)}
+                    />
+                    <div className={styles.actionButtonWrap}>
+                        <Button text="Next" />
+                    </div>
+                    <p className={styles.bottomParagraph}>
+                        By entering your number, you’re agreeing to our Terms of
+                        Service and Privacy Policy. Thanks!
+                    </p>
+                </Card>
+            </div>
+        </>
     );
-}
+};
 
 export default StepOtp;
--- frontend/src/pages/Steps/StepPhoneEmail/StepPhoneEmail.jsx
+++ frontend/src/pages/Steps/StepPhoneEmail/StepPhoneEmail.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Phone from './Phone/Phone';
+import Email from './Email/Email';
 import styles from './StepPhoneEmail.module.css';
 
-const StepPhoneEmail = () => {
+const phoneEmailMap = {
+    phone: Phone,
+    email: Email,
+};
+
+const StepPhoneEmail = ({ onNext }) => {
+    const [type, setType] = useState('phone');
+    const Component = phoneEmailMap[type];
+
     return (
-        <div>StepPhoneEmail</div>
+        <>
+            <div className={styles.cardWrapper}>
+                <div>
+                    <div className={styles.buttonWrap}>
+                        <button
+                            className={`${styles.tabButton} ${
+                                type === 'phone' ? styles.active : ''
+                            }`}
+                            onClick={() => setType('phone')}
+                        >
+                            <img src="/images/phone-white.png" alt="phone" />
+                        </button>
+                        <button
+                            className={`${styles.tabButton} ${
+                                type === 'email' ? styles.active : ''
+                            }`}
+                            onClick={() => setType('email')}
+                        >
+                            <img src="/images/mail-white.png" alt="email" />
+                        </button>
+                    </div>
+                    <Component onNext={onNext} />
+                </div>
+            </div>
+        </>
     );
-}
+};
 
 export default StepPhoneEmail;
